67a035e55bf668190bdcdd4dedca6f569bfadee1
Jacob Bashista <tgb.bashista@gmail.com>
2021-02-20 10:41:01 -0500

    Changed perm name

--- src/main/java/gg/tgb/nospectatorteleports/NoSpectatorTeleports.java
+++ src/main/java/gg/tgb/nospectatorteleports/NoSpectatorTeleports.java
@@ -26,7 +26,7 @@ public final class NoSpectatorTeleports extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         Player p = event.getPlayer();
-        if(p.getGameMode() == GameMode.SPECTATOR && !p.hasPermission("bypassteleportlock")) {
+        if(p.getGameMode() == GameMode.SPECTATOR && !p.hasPermission("bypassteleport")) {
             event.setCancelled(true);
             getLogger().info("Cancelled Teleport for " + p.getDisplayName());
             p.sendMessage("Teleporting is disabled for spectators");
